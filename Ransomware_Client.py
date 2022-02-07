b2fdbcc2138f74832a9883c6276fe0944c42215d
LemonSauc3 <riley@boyder.ca>
2022-02-06 19:08:33 -0500

    Added the first file from the textbook as a python file.

---
+++ Ransomware_Client.py
@@ -0,0 +1,22 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+from cryptography.fernet import Fernet
+
+
+
+
+def main():
+    print("Imports are setup")
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
